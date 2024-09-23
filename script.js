b4d935c5a605bce896ab704ef4175a70d83341fa
josuePDL <135852231+josuePDL@users.noreply.github.com>
2024-09-23 10:07:11 -0600

    Actualizar script.js

--- script.js
+++ script.js
@@ -6,10 +6,10 @@ const loginForm = document.getElementById('loginForm');
 const countDownDate = new Date("October 1, 2024 00:00:00").getTime();
 
 // Frase fija para la contraseña (no se asigna al input)
-const fixedPassword = "Tus ojos son mi universo infinito";
+const fixedPassword = "Gonna love that woman 'til the end of time";
 
 // URL de redirección
-const redirectUrl = "https://sites.google.com/view/lejos-de-casa/p%C3%A1gina-principal";
+const redirectUrl = "https://sites.google.com/view/el-dia-que-todo-cambio/p%C3%A1gina-principal";
 
 // Actualiza la cuenta atrás cada segundo
 const x = setInterval(function() {
